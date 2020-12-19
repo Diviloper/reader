a118b87488ecf346575a5dcd5014a6ca5e2882fe
Christopher Thurston <chris.thurston@gmail.com>
2020-12-18 22:12:34 -0500

    Stdin stream listeners.

---
+++ example/reader_stream_example.dart
@@ -0,0 +1,26 @@
+import 'package:reader/reader.dart';
+
+void main() {
+  CaresAboutStdin();
+
+  print('Type commands "forward 100" or "backward 50" to move the turtle.');
+}
+
+class CaresAboutStdin {
+  CaresAboutStdin() {
+    Reader.stream.addListener(turtleForward);
+    Reader.stream.addListener(turtleBack);
+  }
+
+  void turtleForward(String raw, [List<dynamic> args = const []]) {
+    if (args.length > 1 && args[0] == 'forward' && args[1] is int) {
+      print('Turtle moving forward ${args[1]}');
+    }
+  }
+
+  void turtleBack(String raw, [List<dynamic> args = const []]) {
+    if (args.length > 1 && args[0] == 'back' && args[1] is int) {
+      print('Turtle moving backward ${args[1]}');
+    }
+  }
+}
